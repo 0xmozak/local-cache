69234888096eaa118df6f35afce104fe559a3932
Daniel Bloom <7810950-Daniel.Aaron.Bloom@users.noreply.gitlab.com>
2023-11-18 20:38:17 -0800

    ncc

--- dist/cache/local.d.ts
+++
@@ -1,8 +0,0 @@
-interface CacheResult {
-    cacheKey: string;
-    archiveLocation: string;
-}
-export declare function getLocalCacheEntry(keys: string[]): Promise<CacheResult | undefined>;
-export declare function getLocalArchiveFolder(key: string): Promise<string>;
-export declare function getLocalArchiveFolder(key: string, findKey?: boolean): Promise<string | undefined>;
-export {};
--- dist/cache/tar.d.ts
+++
@@ -1,5 +0,0 @@
-import * as utils from './cacheUtils';
-export declare const getCacheFileName: () => Promise<utils.PosixPath>;
-export declare function listTar(archivePath: utils.PosixPath): Promise<void>;
-export declare function extractTar(archivePath: utils.PosixPath): Promise<void>;
-export declare function createTar(archiveFolder: utils.PosixPath, sourceDirectories: string[]): Promise<void>;
